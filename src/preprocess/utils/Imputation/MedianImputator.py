b46e5a8edfb9ce0e17ed1b907ffcfefd19410190
Yu Chen Su <ycsu2@illinois.edu>
2020-04-18 00:12:48 -0500

    add MedianImputator.

---
+++ src/preprocess/utils/Imputation/MedianImputator.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+import sys
+
+sys.path.append('../../../')
+
+from src.preprocess.utils.Interface.ImputatorInterface import ImputatorInterface
+
+
+class MedianImputator(ImputatorInterface):
+    """
+        Substitute missing value by median value
+    """
+
+    def impute(self, data: pd.DataFrame, column_name: str) -> pd.DataFrame:
+        return data[column_name].fillna((data[column_name].median()), inplace=True)
+
+if __name__ == '__main__':
+    print('### MedianImputator ###')
