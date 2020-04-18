3c82b365b95dc25fa28d51609aef89ef1be35939
whliang2 <whliang2@illinois.edu>
2020-04-18 00:43:42 -0500

    add zeroimpulator

---
+++ src/preprocess/utils/Imputation/ZeroImpulator.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+import sys
+
+sys.path.append('../../../')
+
+from src.preprocess.utils.Interface.ImputatorInterface import ImputatorInterface
+
+
+class ZeroImputator(ImputatorInterface):
+    """
+        Substitute missing value by 0
+    """
+
+    def impute(self, data: pd.DataFrame, column_name: str) -> pd.DataFrame:
+        return data[column_name].fillna(0, inplace=True)
+
+if __name__ == '__main__':
+    print('### MedianImputator ###')
