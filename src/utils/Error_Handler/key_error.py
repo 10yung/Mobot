d051b6a19a25e9b62d5cc5da91d4f8f3bc60bd27
whliang2 <whliang2@illinois.edu>
2020-04-20 00:54:50 -0500

    error handler update

--- src/utils/Error_Handler/key_error.py
+++ src/utils/Error_Handler/key_error.py
@@ -1,4 +1,5 @@
-
+import sys
+import traceback
 
 def safe_run(func):
 
@@ -6,9 +7,14 @@ def safe_run(func):
 
         try:
             func(*args, **kwargs)
-        except Exception as e:
 
-            print(f'Exception type: {e.__class__.__name__}, Invalid param {e}')
+        except Exception as e:
+            ex_type, ex_value, ex_traceback = sys.exc_info()
+            trace_back = traceback.extract_tb(ex_traceback)
+
+            print("Exception type : %s " % ex_type.__name__)
+            print("Exception message : %s" %ex_value)
+            print("Exception file info : %s" %trace_back)
             return None
 
     return func_wrapper
@@ -19,3 +25,8 @@ def add_Num_1(num):
     return(result)
 
 add_Num_1("hi")
+
+
+#
+# for trace in trace_back:
+#     stack_trace.append("File : %s , Line : %d, Func.Name : %s, Message : %s" % (trace[0], trace[1], trace[2], trace[3]))
