52c60a776a82b91dbde800cde28e0769ac670b95
tengyunglin <andynumber10@gmail.com>
2020-04-19 19:23:00 -0500

    chain preprocess funciton

--- src/preprocess/main.py
+++ src/preprocess/main.py
@@ -5,18 +5,64 @@ sys.path.append('../../')
 from src.preprocess.CreateFlatTable import CreateFlatTable
 from src.preprocess.Imputation import Imputation
 from src.preprocess.Transformation import Transformation
-
+from src.preprocess.PreprocessCommandContainer import PreprocessCommandContainer
 
 if __name__ == '__main__':
     print('### Preprocess Main file ###')
 
-    source_directory = '../../data/source'
-    target_directory = '../../data/raw'
-    flat_table = CreateFlatTable(source_directory, target_directory, 'covid_19').exec()
+    preprocess_exec_plan = {
+        'source_dir': '../../data/source',
+        'imputation': [
+            {
+                'columns': ['Pop_Density'],
+                'type': 'mean'
+            },
+            {
+                'columns': ['Literacy....'],
+                'type': 'zero'
+            }
+        ],
+        'transformation': [
+            {
+                'columns': ['Pop_Density'],
+                'type': 'log'
+            },
+            {
+                'columns': ['Literacy....'],
+                'type': 'root_square'
+            }
+        ]
+    }
+
+    preprocess_command_register = PreprocessCommandContainer()
+
+    # register command
+    preprocess_command_register.register('create_flat_from_csv',
+                                         CreateFlatTable(preprocess_exec_plan['source_dir'],
+                                                         {'merge_key': 'Country', 'from_type': 'csv'}))
+    # ----------
+    # register imputation
+    # ----------
+    imputation_types = [el['type'] for el in preprocess_exec_plan['imputation']]
+    for imputation_type in imputation_types:
+        preprocess_command_register.register(f'imputation_by_{imputation_type}', Imputation({'type': imputation_type}))
+
+    # register transform
+    transform_types = [el['type'] for el in preprocess_exec_plan['transformation']]
+    for transform_type in transform_types:
+        preprocess_command_register.register(f'transform_by_{transform_type}', Transformation({'type': transform_type}))
+
+    # ----------
+    # execute command
+    # ----------
+    flat_table = preprocess_command_register.get('create_flat_from_csv').exec()
     print('before preprocess')
     print(flat_table)
 
-    Imputation(['Pop_Density'], flat_table).exec()
-    Transformation(['Pop_Density'], flat_table).exec()
+    for imputation in preprocess_exec_plan['imputation']:
+        preprocess_command_register.get(f"imputation_by_{imputation['type']}").exec(imputation['columns'], flat_table)
+
+    for transform in preprocess_exec_plan['transformation']:
+        preprocess_command_register.get(f"transform_by_{transform['type']}").exec(transform['columns'], flat_table)
     print('after preprocess')
     print(flat_table)
