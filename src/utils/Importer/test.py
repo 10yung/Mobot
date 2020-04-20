da6397dd4ff41894f524eb8cda9ef8af7bfdc233
Yu Chen Su <ycsu2@illinois.edu>
2020-04-20 00:57:58 -0500

    update Importer test

--- src/utils/Importer/test.py
+++ src/utils/Importer/test.py
@@ -4,19 +4,10 @@ sys.path.append('../../../')
 from src.utils.Importer.ImporterManager import ImporterManager
 from src.utils.Importer.ImporterFactory import ImporterFactory
 
-#
-# data = {'First_Name':  ["Henry","Andy","Jane"],
-#         'Last_name': ["Liang", "Lin","Su"],
-#         }
-#
-# df_test = pd.DataFrame(data, columns = ['First_Name','Last_name'])
-source_directory = '../../../data/source'
-
 
 if __name__ == '__main__':
     importer_object = ImporterFactory('csv').generate()
     importer_manager = ImporterManager(importer_object)
-    importer_manager.exec(source_directory)
     files = [{
         'dir': '../../../data/source/',
         'files': ['he_li_pd.csv', 'death_recovery_gdp_unemp_withnan.csv']
@@ -24,6 +15,7 @@ if __name__ == '__main__':
         'dir': '../../../data/preprocessed/',
         'files': ['covid_19.csv']
     }]
-    print(type(importer_manager.exec(files)))
+    importer_manager.exec(files)
+    print(importer_manager.exec(files))
 
 
