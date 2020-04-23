529cff1bf35acade12c46eee8733c056073f014c
whliang2 <whliang2@illinois.edu>
2020-04-22 21:02:34 -0500

    test importer

--- src/split/main.py
+++ src/split/main.py
@@ -7,6 +7,9 @@ from src.split.SplitFactory import SplitFactory
 from src.split.SplitManager import SplitManager
 from src.preprocess.utils.Source.SourceFactory import SourceFactory
 from src.preprocess.utils.Source.SourceManager import SourceManager
+from src.utils.Importer.ImporterFactory import ImporterFactory
+from src.utils.Importer.ImporterManager import ImporterManager
+
 
 
 if __name__ == '__main__':
@@ -43,3 +46,22 @@ if __name__ == '__main__':
     training, testing = SplitManager(ratio_splitter).exec(data, 0.8)
     # print(training)
     # print(testing)
+
+    importer_object = ImporterFactory('csv').generate()
+    importer_manager = ImporterManager(importer_object)
+    files = [{
+        'dir': '../../data/source/',
+        'files': ['he_li_pd.csv', 'death_recovery_gdp_unemp_withnan.csv']
+    }, {
+        'dir': '../../data/preprocessed/',
+        'files': ['covid_19.csv']
+    }]
+    list_from_importer = importer_manager.exec(files)
+    # print(list_from_importer)
+
+
+    df_from_importer = pd.DataFrame(list_from_importer[0])
+    training, testing = SplitManager(ratio_splitter).exec(df_from_importer, 0.8)
+    print(training)
+    print(testing)
+
