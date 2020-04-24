2d070e3e70911ba92d18dbfabb62ad04eba38591
whliang2 <whliang2@illinois.edu>
2020-04-23 23:48:09 -0500

    aic modify

--- src/estimate/RMSE/rmse.py
+++ src/estimate/RMSE/rmse.py
@@ -8,7 +8,7 @@ from src.preprocess.utils.Source.SourceFactory import SourceFactory
 from src.preprocess.utils.Source.SourceManager import SourceManager
 from src.utils.Importer.ImporterFactory import ImporterFactory
 from src.utils.Importer.ImporterManager import ImporterManager
-from math import log
+from math import log,sqrt
 from sklearn.datasets import make_regression
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
@@ -18,7 +18,7 @@ from itertools import combinations
 from src.models.AIC.aic import aic
 from src.models.SimpleLm.SimpleLm import SimpleLm
 from src.models.StepWise.StepWise import StepWise
-
+from sklearn.metrics import mean_squared_error
 
 
 
@@ -41,8 +41,8 @@ if __name__ == '__main__':
     selected_column_list =['health_expend', 'literacy', 'physicians_density', 'obesity',
            'life_expect', 'h_bed_density', 'imigrate_rate']
 
-    aic_object = aic()
-    aic_model, aic_predictor, aic_result_df = aic.exec( training , selected_column_list, ['recovery_rate'])
+    # aic_object = aic()
+    # aic_model, aic_predictor, aic_result_df = aic.exec( training , selected_column_list, ['recovery_rate'])
 
     criteria = {
         'p_value': 0.05
@@ -55,6 +55,14 @@ if __name__ == '__main__':
     simple_model, simple_predictor, simple_result_df = model.exec(training, selected_column_list, ['recovery_rate'])
 
 
-    print(aic_model)
-    print(stepwise_model)
+    # print(aic_model)
+    print(stepwise_result_df)
     print(simple_model)
+
+    # aic_y_pred = aic_model.predict(testing[aic_predictor])
+    # print(aic_y_pred)
+    # aic_rms = sqrt(mean_squared_error(testing['recovery_rate'], aic_y_pred))
+    # print(aic_rms)
+
+    # ypred = olsres.predict(X)
+
