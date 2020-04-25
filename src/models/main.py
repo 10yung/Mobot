10054d3be7cb938a3a0c7342027e9f5269593cca
tengyunglin <andynumber10@gmail.com>
2020-04-24 22:39:14 -0500

    refactor, add model register object to wrap model execute plan

--- src/models/main.py
+++ src/models/main.py
@@ -1,10 +1,11 @@
+import pandas as pd
 import sys
 sys.path.append('../../')
 
-import pandas as pd
 from src.split.SplitFactory import SplitFactory
 from src.split.SplitManager import SplitManager
-from src.models.AIC.aic import aic
+from src.models.ModelRegister import ModelRegister
+from src.models.AIC.AIC import AIC
 from src.models.SimpleLm.SimpleLm import SimpleLm
 from src.models.StepWise.StepWise import StepWise
 from src.utils.Exporter.ExportFactory import ExportFactory
@@ -21,8 +22,10 @@ if __name__ == '__main__':
         'response_name': ['Recovery Rate'],
         'experiments': [{
             'name': 'AIC',
+            'criteria': {}
         }, {
             'name': 'SimpleLm',
+            'criteria': {}
         }, {
             'name': 'StepWise',
             'criteria': {
@@ -51,47 +54,75 @@ if __name__ == '__main__':
     }]
 
     data = importer_manager.exec(files)[0]
-
     # initialize split object
     ratio_splitter = SplitFactory('ratio').generate()
     training, testing = SplitManager(ratio_splitter).exec(data, 0.8)
 
+
+    # register models
+    model_register = ModelRegister()
+
+    model_map = {
+        'AIC': AIC,
+        'StepWise': StepWise,
+        'SimpleLm': SimpleLm
+    }
+
+    for model in model_exec_plan['experiments']:
+        model_name = model['name']
+        model_register.register(model_name, model_map[model_name],model_exec_plan['predictor_name'], model_exec_plan['response_name'] )
+
+    # execute models base on execution plan
     model_table = {}
     for model in model_exec_plan['experiments']:
-        # AIC
-        if model['name'] == 'AIC':
-            aic_object = aic()
-            aic_result_list = aic.exec(training, model_exec_plan['predictor_name'],
-                                       model_exec_plan['response_name'])
-            aic_result_model = model['name']
-            aic_result_x = aic_result_list[1]
-            aic_result_x = ' '.join(aic_result_x)
-            aic_result_y = model_exec_plan['response_name'][0]
-            model_table[aic_result_model] =[aic_result_y, aic_result_x,'']
+        result = model_register.exec(model['name'], training, model['criteria'])
 
-        # Simple Linear Regression
-        elif model['name'] == 'SimpleLm':
-            simple_object = SimpleLm()
-            simple_result_list = simple_object.exec(data, model_exec_plan['predictor_name'],
-                                                    model_exec_plan['response_name'])
-            simple_result_model = model['name']
-            simple_result_x = simple_result_list[1]
-            simple_result_x = ' '.join(simple_result_x)
-            simple_result_y = model_exec_plan['response_name'][0]
-            model_table[simple_result_model] =[simple_result_y, simple_result_x,'']
-
-        # StepWise
-        elif model['name'] == 'StepWise':
-            stepwise_object = StepWise(model['criteria'])
-            sw_result_list = stepwise_object.exec(training, model_exec_plan['predictor_name'],
-                                                  model_exec_plan['response_name'])
+        if len(model['criteria'].items()) == 0:
+            criteria = ''
+            model_name = model['name']
+        else:
             key, value = list(model['criteria'].items())[0]
-            simple_result_model = model['name']+ '_' + str(value)
-            sw_result_x = sw_result_list[1]
-            sw_result_x = ' '.join(sw_result_x)
-            sw_result_y = model_exec_plan['response_name'][0]
-            p_val = key + ' ' + str(value)
-            model_table[simple_result_model] =[sw_result_y, sw_result_x, p_val]
+            criteria = key + ' ' + str(value)
+            model_name = model['name'] + '_' + str(value)
+
+        model_table[model_name] =[model_exec_plan['response_name'], ' '.join(result[1]), criteria]
+
+
+#     for model in model_exec_plan['experiments']:
+#         # AIC
+#         if model['name'] == 'AIC':
+#             aic_object = AIC()
+#             aic_result_list = aic_object.exec(training, model_exec_plan['predictor_name'],
+#                                        model_exec_plan['response_name'])
+#             aic_result_model = model['name']
+#             aic_result_x = aic_result_list[1]
+#             aic_result_x = ' '.join(aic_result_x)
+#             aic_result_y = model_exec_plan['response_name'][0]
+#             model_table[aic_result_model] =[aic_result_y, aic_result_x,'']
+#
+#         # Simple Linear Regression
+#         elif model['name'] == 'SimpleLm':
+#             simple_object = SimpleLm()
+#             simple_result_list = simple_object.exec(data, model_exec_plan['predictor_name'],
+#                                                     model_exec_plan['response_name'])
+#             simple_result_model = model['name']
+#             simple_result_x = simple_result_list[1]
+#             simple_result_x = ' '.join(simple_result_x)
+#             simple_result_y = model_exec_plan['response_name'][0]
+#             model_table[simple_result_model] =[simple_result_y, simple_result_x,'']
+#
+#         # StepWise
+#         elif model['name'] == 'StepWise':
+#             stepwise_object = StepWise(model['criteria'])
+#             sw_result_list = stepwise_object.exec(training, model_exec_plan['predictor_name'],
+#                                                   model_exec_plan['response_name'])
+#             key, value = list(model['criteria'].items())[0]
+#             simple_result_model = model['name']+ '_' + str(value)
+#             sw_result_x = sw_result_list[1]
+#             sw_result_x = ' '.join(sw_result_x)
+#             sw_result_y = model_exec_plan['response_name'][0]
+#             p_val = key + ' ' + str(value)
+#             model_table[simple_result_model] =[sw_result_y, sw_result_x, p_val]
 
     model_table = pd.DataFrame.from_dict(model_table, orient='index', columns=['response', 'predictors', 'criteria'])
     model_table = model_table.reset_index()
