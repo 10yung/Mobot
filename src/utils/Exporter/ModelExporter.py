920fa42092cfdc95c427f7adb4952fa554392b67
tengyunglin <andynumber10@gmail.com>
2020-04-27 22:07:52 -0500

    change model exporter save function

--- src/utils/Exporter/ModelExporter.py
+++ src/utils/Exporter/ModelExporter.py
@@ -1,6 +1,5 @@
 import sys
-import os
-import pandas as pd
+import pickle
 sys.path.append('../../')
 
 from src.utils.Exporter.ExportInterface import ExportInterface
@@ -13,11 +12,20 @@ class ModelExporter(ExportInterface):
     @staticmethod
     def save_file(target: object, target_dir: str, file_name: str):
         """
-        Save the model object to static file in the models folder
+        Save the model object to static file in the models folder by identify the package name
         :param target: model object <statsmodel> <skilearn>
         :param target_dir: string 'project model folder'
         :param file_name: string 'model name'
         :return:
         """
-        target.save(f'{target_dir}/{file_name}')
-        print('Model saved!')
+        package_name = target.__class__.__module__.split(".")[0]
+
+        if package_name == 'statsmodels':
+
+            target.save(f'{target_dir}/{file_name}')
+            print('statsmodels saved!')
+        elif package_name == 'sklearn':
+            # save sklearn model
+            with open(f'{target_dir}/{file_name}.pkl', 'wb') as file:
+                pickle.dump(target, file)
+            print('sklearn saved!')
